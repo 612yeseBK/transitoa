6ea2cac9e65b2ab1d8dc000fef2c6c81c01c71b9
612yese <240590367@qq.com>
2018-05-23 11:29:45 +0800

    pojo

--- src/main/java/com/expect/admin/data/pojo/Addwf.java
+++ src/main/java/com/expect/admin/data/pojo/Addwf.java
@@ -8,9 +8,10 @@ import java.util.List;
  */
 public class Addwf {
     private String name;//流程名称
-    private String type;//流程名称
+    private String type;//流程类别
     private String description;//节点描述
     private List<String> wfpnames;//该流程的流程节点们，list的顺序就是流程节点的顺序
+    private List<String> funcIds;//不同流程节点对应的角色功能，这两个list长度必须相同,其一一对应
 
     public String getName() {
         return name;
@@ -42,5 +43,13 @@ public class Addwf {
 
     public void setWfpnames(List<String> wfpnames) {
         this.wfpnames = wfpnames;
+    }
+
+    public List<String> getFuncIds() {
+        return funcIds;
+    }
+
+    public void setFuncIds(List<String> funcIds) {
+        this.funcIds = funcIds;
     }
 }
